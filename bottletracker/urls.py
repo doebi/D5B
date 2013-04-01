fc12ed20199279908fa02bb0f02a69e59063226b
doebi <christoph@doeberl.at>
2013-04-01 20:20:46 +0200

    Added some raw views

--- bottletracker/urls.py
+++ bottletracker/urls.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'bottletracker.views.home', name='home'),
-    # url(r'^bottletracker/', include('bottletracker.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+        url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += patterns('event.views',
+    url(r'^$', 'index'),
+        url(r'^track/(?P<user_id>\w+)/(?P<brand>\w+)/$', 'track'),
+        url(r'^deposit/$', 'deposit'),
+        url(r'^user/(?P<user_id>\w+)/$', 'user'),
+        url(r'^user/$', 'userlist'),
+        url(r'^stats/$', 'stats'),
+)
+
--- event/views.py
+++ event/views.py
@@ -1,1 +1,43 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render_to_response, render
+from django.contrib.auth.models import User
+from event.models import Product, Event
+from django.db.models import Sum
+from decimal import *
+
+def index(request):
+    return render(request, 'index.html')
+
+def track(request, user_id, brand):
+    return HttpResponse("tracking!")
+
+def deposit(request):
+    data = list()
+    for p in Product.objects.all():
+        sumobject = Event.objects.filter(product=p).aggregate(Sum('amount'))
+        if not sumobject['amount__sum']:
+            sumobject['amount__sum'] = 0
+        data.append({'name': p.name, 'amount': sumobject['amount__sum']})
+    return render(request, 'deposit.html', {'products': data})
+
+def stats(request):
+    return HttpResponse("stats page")
+
+def user(request, user_id):
+    u = User.objects.get(username=user_id)
+    data = {'name': u.username, 'sum': Decimal(0)}
+    for a in Event.ACTIONS:
+        obj = Event.objects.filter(user=u, action=a[0]).aggregate(Sum('value'))
+        value = obj['value__sum']
+        if not value:
+            value = Decimal(0)
+        data[a[0]] = value
+        if a[0] == 'C':
+            data['sum'] -= value
+        else:
+            data['sum'] += value
+    return render(request, 'user.html', {'data': data})
+
+def userlist(request):
+    users = User.objects.all()
+    return render(request, 'userlist.html', {'users': users})
